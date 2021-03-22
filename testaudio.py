df81ad5cafed766e569161ac2df360f0619ae1aa
Yueying-12N <yueying.cao@eleves.ec-nantes.fr>
2021-03-23 04:32:20 +0800

    change the path of audio

--- testaudio.py
+++ testaudio.py
@@ -8,7 +8,7 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 
-wf = wave.open(r'E:\毕设\代码\LedZeppelin.wav','rb')
+wf = wave.open(r'E:\毕设\代码\Project_sd1\LedZeppelin.wav','rb')
 p = pyaudio.PyAudio()
 
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
@@ -26,7 +26,7 @@ stream.close()
 p.terminate()
 
 
-wf = wave.open('E:\毕设\代码\LedZeppelin.wav','rb')
+wf = wave.open('E:\毕设\代码\Project_sd1\LedZeppelin.wav','rb')
 params = wf.getparams()
 nchannels,sampwidth,framerate,nframes=params[:4]
 str_data = wf.readframes(nframes)
@@ -35,6 +35,3 @@ wave_data = np.fromstring(str_data,dtype=np.short)
 plt.figure()
 plt.plot(wave_data)
 plt.show()
-
-
-'''test'''
